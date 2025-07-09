0f60fa2c47a969c0698073fa624898753d19166c
Niklas Maroldt <nik.dev@gmx.net>
2025-07-09 23:13:54 +0200

    add method: get_uuid

--- skinviewer/app.py
+++ skinviewer/app.py
@@ -1,1 +1,5 @@
 import get_mojangapi_request
+
+playername = input("Enter a playername to search for: ")
+uuid = get_mojangapi_request.get_uuid(playername)
+print(uuid)
--- skinviewer/get_mojangapi_request.py
+++ skinviewer/get_mojangapi_request.py
@@ -1,5 +1,14 @@
+import requests
+
 def get_uuid(playername):
-    pass
+    base_url = "https://api.minecraftservices.com/minecraft/profile/lookup/name/"
+    response = requests.get(base_url + playername)
+    data = response.json()
+    if "id" in data:
+        uuid = data["id"]
+        return uuid
+    else:
+        print(f"[WARN] No player with name '{playername}' found.")
 
 def get_player_data(uuid):
     pass
