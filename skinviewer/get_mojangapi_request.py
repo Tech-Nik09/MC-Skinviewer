e37299457edcbebd1160b56b8bb2b8d7d81616cb
Niklas Maroldt <nik.dev@gmx.net>
2025-07-10 12:19:17 +0200

    add method: get_skin_url

--- skinviewer/get_mojangapi_request.py
+++ skinviewer/get_mojangapi_request.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import base64
 
 def get_uuid(playername):
     base_url = "https://api.minecraftservices.com/minecraft/profile/lookup/name/"
@@ -10,5 +12,11 @@ def get_uuid(playername):
     else:
         print(f"[WARN] No player with name '{playername}' found.")
 
-def get_player_data(uuid):
-    pass
+def get_skin_url(uuid):
+    base_url = "https://sessionserver.mojang.com/session/minecraft/profile/"
+    response = requests.get(base_url + uuid)
+    data = response.json()
+    value_encoded = data["properties"][0]["value"]
+    value_decoded = base64.b64decode(value_encoded)
+    skin_url = json.loads(value_decoded)["textures"]["SKIN"]["url"]
+    return skin_url
